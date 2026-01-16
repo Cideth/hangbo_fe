4aee249adbb9c6019cd1a2892a040f42ef28648f
dusdn <vpdkzl4159@gmail.com>
2026-01-16 15:26:41 +0900

    Update page and hero components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Nav from "@/components/Home/Nav";
 import Hero from "@/components/Home/Hero";
-import Features from "@/components/Home/Features";
 import Footer from "@/components/Home/Footer";
 import Login from "@/components/Login/page";
 
@@ -15,7 +14,6 @@ export default function HomePage() {
     <>
       <Nav />
       <Hero />
-      <Features />
       <Footer />
     </>
   );
